0ada80d21c81e3326ea41942dcb7692fb0640bd6
twhs <twhs768@gmail.com>
2014-09-10 11:52:20 +0900

    Finish twitter #4

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,4 +18,28 @@ class SessionsController < ApplicationController
     sign_out
     redirect_to root_url
   end
+
+  def twitter_create
+      auth = request.env["omniauth.auth"]
+      puts"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+      puts auth["info"]["nickname"]
+      puts auth["info"]["image"]
+      puts auth["info"]["urls"]["Twitter"]
+      puts"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+      user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+      session[:user_id] = user.id
+      session[:screen_name] = auth["info"]["nickname"]
+      session[:twitter_icon] = auth["info"]["image"]
+      session[:twitter_url] = auth["info"]["urls"]["Twitter"]
+      session[:oauth_token] = auth.credentials.token
+      session[:oauth_token_secret] = auth.credentials.secret
+      sign_in user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to root_url
+  end
+
+  def twitter_destroy
+        session[:user_id] = nil
+          redirect_to root_url, :notice => "Signed out!"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140910010706) do
+ActiveRecord::Schema.define(version: 20140910024616) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
@@ -44,9 +44,12 @@ ActiveRecord::Schema.define(version: 20140910010706) do
     t.string   "remember_token"
     t.boolean  "admin",           default: false
     t.string   "image"
+    t.string   "provider",        default: "",    null: false
+    t.string   "uid",             default: "",    null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
+  add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
 
 end
